ed7f08ee05e96c6d893b0ec927bbf096066f3a9f
eladjmc <eladjmc88@gmail.com>
2023-12-18 14:59:34 +0200

    updated student file

--- Students/mastermind.js
+++ Students/mastermind.js
@@ -16,49 +16,69 @@
 
 
 
-// Function: getRandomInt(min, max)
-// Write a function that generates a random integer between 'min' (inclusive) and 'max' (exclusive)
+// Function to generate a random number within a range
+function getRandomInt(min, max) {
+    // Step 1: Return a random integer between min (inclusive) and max (exclusive)
+}
 
+// Function to create a secret code
+function createSecretCode() {
+    // Step 1: Initialize an empty array named 'code'
+    // Step 2: Use a loop to generate 4 random numbers and add them to the 'code' array
+    // Step 3: Return the 'code' array
+}
 
+// Function to prompt user input with validation
+function promptUser(index) {
+    // Step 1: Initialize a variable 'guess'
+    // Step 2: Use a loop to repeatedly prompt the user until a valid input is received
+    // Step 3: Return the valid guess
+}
 
-// Function: createSecretCode()
-// Write a function to create the secret code for the game
-// The function should return an array of random numbers, each between 1 and 4
+// Function to get user guesses
+function getUserGuesses() {
+    // Step 1: Initialize an empty array named 'guesses'
+    // Step 2: Use a loop to get 4 guesses from the user and add them to the 'guesses' array
+    // Step 3: Return the 'guesses' array
+}
 
+// Function to check user guess against secret code
+function checkGuess() {
+    // Step 1: Reset checkGuessResult, totalScore, and correctAnswers for the new round
+    // Step 2: Use a loop to compare each user guess with the secret code
+    // Step 3: Update checkGuessResult, totalScore, and correctAnswers based on the comparison
+}
 
+// Function to get user color pictures based on guesses
+function getUserColorPictures() {
+    // Step 1: Initialize an empty array named 'pictureFilenames'
+    // Step 2: Use a loop to iterate over userGuessArray
+    // Step 3: Use a switch case inside the loop to determine the filename for each guess and add it to 'pictureFilenames'
+    // Step 4: Return the 'pictureFilenames' array
+}
 
-// Function: promptUser(index)
-// Write a function to prompt the user for their guess for a given color
-// The function should validate the input to ensure it's a number between 1 and 4
+// Function to check if the user has won the game
+function checkWin() {
+    // Step 1: Check if the number of correctAnswers equals 4 to determine a win
+    // Step 2: Show a win alert message if the user wins
+}
 
+// Function to start a guess round
+function startGuessRound() {
+    // Step 1: Get user guesses and store them in userGuessArray
+    // Step 2: Call checkGuess to evaluate the guesses
+    // Step 3: Get picture filenames based on guesses and store them in pictureNames
+    // Step 4: Update the game board by calling addGuessRow and addFeedbackRow
 
+    // the helper functions will update the screen
+    addGuessRow() 
+    addFeedbackRow()
 
-// Function: getUserGuesses()
-// Write a function to get all user guesses for a round
-// The function should call promptUser for each guess in the round and return an array of guesses
-
-
-
-// Function: checkGuess()
-// Write a function to check the user's guess against the secret code
-// The function should reset the round's score and check each guess, updating totalScore and correctAnswers
-
-
-
-
-// Function: getUserColorPictures()
-// Write a function to get the filenames of images based on user guesses
-// The function should map each guess to the corresponding image filename
-
-
-
-// Function: checkWin()
-// Write a function to check if the user has won the game
-// The function should display a win message if the user guesses all colors correctly
+    // Step 5: Check if the user has won the game by calling checkWin
+}
 
 
 // Helper Functions - You need to use this function to display result to the screen. You don't need to change them.
-
 // Function to add a guess row to the table
 function addGuessRow() {
     let table = document.getElementById("guessBoard");
